703638a60e7df56a60441e5d2d41db0cf9750fa0
Lê Quang Nhựt <nhut.thanthien17@gmail.com>
2024-12-15 02:14:53 +0700

    refactor: remove img, config img

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
+    domains: ["res.cloudinary.com", "localhost"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
       },
     ],
     unoptimized: true,
